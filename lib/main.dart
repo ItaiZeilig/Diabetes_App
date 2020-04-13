cc52774fac5a1e493a3b3abe0523dd8639b12e52
ItaiZeilig <itai.zeilig@s.afeka.ac.il>
2020-04-13 21:27:53 +0300

    Add files to git

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:diabetes_app/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './service/auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'DoctorApp',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            // Define the default brightness and colors.
+            brightness: Brightness.light,
+            primaryColor: Color(0xFF7f70e7),
+            accentColor: Color(0xFF7f70e7),
+          ),
+          home: LoginScreen(),
+          routes: {
+            LoginScreen.routeName: (ctx) => LoginScreen(),
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,252 @@
+import 'package:diabetes_app/service/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+enum AuthMode { Signup, Login }
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login';
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  Auth auth;
+  AuthMode _authMode = AuthMode.Login;
+  final _formKey = GlobalKey<FormState>();
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+    'name': '',
+  };
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An Error Occurred!'),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      // Invalid!
+      return;
+    }
+    _formKey.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      if (_authMode == AuthMode.Login) {
+        // Login User
+        await auth.signIn(
+            _authData['email'].trim(), _authData['password'].trim());
+      } else {
+        // Sign user up
+        // await auth.signUp(_authData['email'].trim(),
+        //     _authData['password'].trim(), _authData['name']);
+      }
+    } catch (error) {
+      _showErrorDialog(error.message);
+      setState(() {
+        _formKey.currentState.reset();
+      });
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _switchAuthMode() {
+    if (_authMode == AuthMode.Login) {
+      setState(() {
+        _authMode = AuthMode.Signup;
+      });
+    } else {
+      setState(() {
+        _authMode = AuthMode.Login;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    auth = Provider.of<Auth>(context);
+    final deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(40),
+              alignment: Alignment.center,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Image.asset('assets/images/logo.png'),
+                    width: 120,
+                    height: 120,
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          cursorColor: Theme.of(context).primaryColor,
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            labelText: 'E-Mail',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value.isEmpty || !value.contains('@')) {
+                              return 'Invalid email!';
+                            }
+                          },
+                          onSaved: (value) {
+                            _authData['email'] = value;
+                          },
+                        ),
+                        TextFormField(
+                          cursorColor: Theme.of(context).primaryColor,
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            labelText: 'Password',
+                          ),
+                          controller: _passwordController,
+                          obscureText: true,
+                          onFieldSubmitted: (_) {
+                            _submit();
+                          },
+                          validator: (value) {
+                            if (value.isEmpty || value.length < 5) {
+                              return 'Password is too short!';
+                            }
+                          },
+                          onSaved: (value) {
+                            _authData['password'] = value;
+                          },
+                        ),
+                        if (_authMode == AuthMode.Signup)
+                          Column(
+                            children: <Widget>[
+                              TextFormField(
+                                enabled: _authMode == AuthMode.Signup,
+                                decoration: InputDecoration(
+                                    labelStyle: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    labelText: 'Confirm Password'),
+                                obscureText: true,
+                                validator: _authMode == AuthMode.Signup
+                                    ? (value) {
+                                        if (value != _passwordController.text) {
+                                          return 'Passwords do not match!';
+                                        }
+                                      }
+                                    : null,
+                              ),
+                       TextFormField(
+                          cursorColor: Theme.of(context).primaryColor,
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            labelText: 'Display Name',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value.isEmpty ) {
+                              return 'Invalid Display Name!';
+                            }
+                          },
+                          onSaved: (value) {
+                            _authData['name'] = value;
+                          },
+                        ),
+                            ],
+                          ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        if (_isLoading)
+                          CircularProgressIndicator()
+                        else
+                          ButtonTheme(
+                            minWidth: 300.0,
+                            child: RaisedButton(
+                              color: Color(0xFFc1b7f3),
+                              onPressed: _submit,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Text(
+                                '${_authMode == AuthMode.Login ? 'LOGIN' : 'SIGNUP'}',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            MaterialButton(
+                              onPressed: _switchAuthMode,
+                              splashColor: Theme.of(context).primaryColor,
+                              child: Text(
+                                'SignUp',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                            MaterialButton(
+                              onPressed: () {},
+                              splashColor: Theme.of(context).primaryColor,
+                              child: Text(
+                                'Forgot password',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Container(
+            //   alignment: Alignment.bottomCenter,
+            //   child: BottomWaveWidget(),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
