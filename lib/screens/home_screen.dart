f9defc3492c91df19d655237eda68122159459ef
ItaiZeilig <itayzelig@hotmail.com>
2020-05-13 12:28:55 +0300

    Added drawer to home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diabetes_app/providers/auth.dart';
 import 'package:diabetes_app/providers/user_service.dart';
 import 'package:diabetes_app/screens/daily_challenge_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:diabetes_app/widgets/home_screen/category.dart';
+import 'package:diabetes_app/widgets/home_screen/news_headline.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
@@ -14,8 +16,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Auth _auth;
-
   var _isLoading = true;
+  GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey<ScaffoldState>();
 
   @override
   void didChangeDependencies() {
@@ -32,96 +34,136 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldkey,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            ListTile(
+              title: Text("Logout"),
+              onTap: () {
+                _auth.logOut();
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            _scaffoldkey.currentState.openDrawer();
+          },
+        ),
+      ),
       body: _isLoading
           ? Center(
               child: CircularProgressIndicator(),
             )
           : SafeArea(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Hello,",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 20),
+                            ),
+                            Consumer<UserService>(
+                              builder: (context, user, child) {
+                                return Text(
+                                  user.getUser.name,
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
                     children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "Hello,",
-                            style:
-                                TextStyle(color: Colors.grey, fontSize: 20),
-                          ),
-                          Consumer<UserService>(
-                            builder: (context, user, child) {
-                              return Text(
-                                user.getUser.name,
-                                style: TextStyle(
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.bold),
-                              );
-                            },
-                          ),
-                        ],
+                      Text(
+                        "Categories",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
                       ),
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            "https://p7.hiclipart.com/preview/14/65/239/ico-avatar-scalable-vector-graphics-icon-doctor-with-stethoscope.jpg"),
-                      ),
-                      MaterialButton(
-                        onPressed: () => _auth.logOut(),
-                        splashColor: Theme.of(context).primaryColor,
-                        child: Text(
-                          'Sign Out',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                          ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        height: 100,
+                        child: ListView(
+                          // This next line does the trick.
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            SingleCategory(
+                                "Daily Challenges",
+                                DailyChallenge.routeName,
+                                "daily_challenges_icon.png"),
+                            SingleCategory("Chat", DailyChallenge.routeName,
+                                "chat_icon.png"),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      "Categories",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.bold),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "Today News",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: Column(
+                            children: <Widget>[
+                              SingleNewsHeadline(
+                                  "Why need to check up?",
+                                  Timestamp.now(),
+                                  4.5,
+                                  "daily_challenges_icon"),
+                              SingleNewsHeadline(
+                                  "Why need to check up?",
+                                  Timestamp.now(),
+                                  4.5,
+                                  "daily_challenges_icon"),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      height: 100,
-                      child: ListView(
-                        // This next line does the trick.
-                        scrollDirection: Axis.horizontal,
-                        children: <Widget>[
-                          SingleCategory(
-                              "Daily Challenges",
-                              DailyChallenge.routeName,
-                              "daily_challenges_icon.png"),
-                          SingleCategory("Chat", DailyChallenge.routeName,
-                              "chat_icon.png"),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      "Today News",
-                      style: TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: Text("hello"),
-                    ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
     );
   }
 }
